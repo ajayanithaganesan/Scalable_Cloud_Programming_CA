bbcd2b4afadbeb1fe1fa846cf9c25c2a98d96abe
ajayanithaganesan <agajay99@gmail.com>
2026-07-10 00:23:59 +0100

    Setup Kinesis stream configuration and ingestion environment

---
+++ ingestion/config.py
@@ -0,0 +1,13 @@
+# AWS Region
+AWS_REGION = "us-east-1"
+
+# Kinesis Stream Name
+KINESIS_STREAM = "crypto-stream"
+
+# Binance WebSocket URL
+BINANCE_WS = (
+    "wss://stream.binance.com:443/stream?"
+    "streams=btcusdt@trade/"
+    "ethusdt@trade/"
+    "solusdt@trade"
+)
